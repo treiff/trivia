900b3eda6469f285fb5a4427b3135ed89ae4989a
Trevor Reiff <trevorreiff@gmail.com>
2017-07-30 20:56:08 -0400

    Add logic to make request.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,15 @@
 extern crate curl;
 
+use std::io::{stdout, Write};
+use curl::easy::Easy;
+
 fn main() {
+    let mut handle = Easy::new();
+
+    handle.url("https://opentdb.com/api.php?amount=10").unwrap();
+
+    handle.write_function(|data| {
+        Ok(stdout().write(data).unwrap())
+    }).unwrap();
+    handle.perform().unwrap();
 }
