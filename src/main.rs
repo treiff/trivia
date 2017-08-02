5ff9e7a0c4472c27452af41b289e20236c6300d2
Trevor Reiff <trevorreiff@gmail.com>
2017-08-01 21:27:52 -0400

    Modify to store json string in variable.

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,23 @@
 extern crate curl;
+extern crate serde_json;
 
-use std::io::{stdout, Write};
+use std::str;
 use curl::easy::Easy;
 
 fn main() {
     let mut handle = Easy::new();
+    let mut json_string = String::new();
 
     handle.url("https://opentdb.com/api.php?amount=10").unwrap();
 
-    handle.write_function(|data| {
-        Ok(stdout().write(data).unwrap())
-    }).unwrap();
-    handle.perform().unwrap();
+    {
+        let mut transfer = handle.transfer();
+        transfer.write_function(|data| {
+            json_string.push_str(str::from_utf8(data).unwrap());
+            Ok(data.len())
+        }).unwrap();
+        transfer.perform().unwrap();
+    }
+
+    println!("string is: {0}", json_string);
 }
